545053213fdccec262a8c43811f2ab422ecfce3c
AliceyXia <xiay424@newschool.edu>
2023-10-02 17:55:40 -0400

    illustration

--- function.js
+++ function.js
@@ -1,13 +1,14 @@
 document.getElementById("color-block").onclick = function () {
     changeColor();
   };
+
   function changeColor() {
     if (
-      (document.getElementById("color-block").style.backgroundColor == "rgb(255, 255, 255)")
+      (document.getElementById("color-block").style.backgroundColor == "black")
     ) {
-      document.getElementById("color-block").style.backgroundColor = "black";
+      document.getElementById("color-block").style.backgroundColor = "rgb(0, 255, 0)";
     } else {
-      document.getElementById("color-block").style.backgroundColor = "rgb(255, 255, 255)";
+      document.getElementById("color-block").style.backgroundColor = "black";
     }
   }
   
